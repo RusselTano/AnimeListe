bb6c65d6dd515397c5045aa5ab48c3580eeb9cad
manuela <emkengmo02@monccnb.ca>
2024-10-30 10:14:57 -0300

    final

--- AnimeListeForm.Designer.cs
+++ AnimeListeForm.Designer.cs
@@ -28,16 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnimeListeForm));
             this.animeListeMenuStrip = new System.Windows.Forms.MenuStrip();
             this.policesToolStripComboBox = new System.Windows.Forms.ToolStripComboBox();
             this.typeAnimeLabel = new System.Windows.Forms.Label();
             this.typeAnimeRichTextBox = new System.Windows.Forms.RichTextBox();
             this.imprimerButton = new System.Windows.Forms.Button();
             this.rechercheGroupBox = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.rechercheButton = new System.Windows.Forms.Button();
             this.rechercherAnimeTextBox = new System.Windows.Forms.TextBox();
             this.rechercherAnimelabel = new System.Windows.Forms.Label();
             this.animeListePictureBox = new System.Windows.Forms.PictureBox();
+            this.animeListePrintDocument = new System.Drawing.Printing.PrintDocument();
+            this.animeListeFontDialog = new System.Windows.Forms.FontDialog();
+            this.animeListePrintPreviewDialog = new System.Windows.Forms.PrintPreviewDialog();
             this.animeListeMenuStrip.SuspendLayout();
             this.rechercheGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.animeListePictureBox)).BeginInit();
@@ -45,103 +49,128 @@
             // 
             // animeListeMenuStrip
             // 
+            this.animeListeMenuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.animeListeMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.animeListeMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.policesToolStripComboBox});
             this.animeListeMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.animeListeMenuStrip.Name = "animeListeMenuStrip";
-            this.animeListeMenuStrip.Size = new System.Drawing.Size(577, 32);
+            this.animeListeMenuStrip.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.animeListeMenuStrip.Size = new System.Drawing.Size(793, 44);
             this.animeListeMenuStrip.TabIndex = 0;
             this.animeListeMenuStrip.Text = "menuStrip1";
             // 
             // policesToolStripComboBox
             // 
-            this.policesToolStripComboBox.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.policesToolStripComboBox.Name = "policesToolStripComboBox";
-            this.policesToolStripComboBox.Size = new System.Drawing.Size(121, 28);
+            this.policesToolStripComboBox.Size = new System.Drawing.Size(165, 38);
+            this.policesToolStripComboBox.SelectedIndexChanged += new System.EventHandler(this.policesToolStripComboBox_SelectedIndexChanged);
             // 
             // typeAnimeLabel
             // 
             this.typeAnimeLabel.AutoSize = true;
-            this.typeAnimeLabel.Location = new System.Drawing.Point(29, 239);
+            this.typeAnimeLabel.Location = new System.Drawing.Point(40, 358);
+            this.typeAnimeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.typeAnimeLabel.Name = "typeAnimeLabel";
-            this.typeAnimeLabel.Size = new System.Drawing.Size(358, 16);
+            this.typeAnimeLabel.Size = new System.Drawing.Size(521, 25);
             this.typeAnimeLabel.TabIndex = 2;
             this.typeAnimeLabel.Text = "Type d\'anime : (selection indique l\'anime le meilleur anime)";
             // 
             // typeAnimeRichTextBox
             // 
-            this.typeAnimeRichTextBox.Location = new System.Drawing.Point(32, 269);
+            this.typeAnimeRichTextBox.Location = new System.Drawing.Point(44, 404);
+            this.typeAnimeRichTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.typeAnimeRichTextBox.Name = "typeAnimeRichTextBox";
-            this.typeAnimeRichTextBox.Size = new System.Drawing.Size(521, 142);
+            this.typeAnimeRichTextBox.Size = new System.Drawing.Size(715, 211);
             this.typeAnimeRichTextBox.TabIndex = 3;
-            this.typeAnimeRichTextBox.Text = "Shōnen\t\tDragon Ball \t\nShōnen\t\tNaruto\nShōnen\t\tOne Piece\nShōnen\t\tBleach \nShōnen\t\tMy" +
-    " Hero Academia \nShojo\t\tFruits Basket\nShojo\t\tCardcaptor Sakura\nShojo\t\tNana\nShojo\t" +
-    "\tAo Haru Ride\nShojo\t\tKimi ni Todoke\n\t";
+            this.typeAnimeRichTextBox.Text = "";
             // 
             // imprimerButton
             // 
-            this.imprimerButton.Location = new System.Drawing.Point(32, 427);
+            this.imprimerButton.Location = new System.Drawing.Point(44, 640);
+            this.imprimerButton.Margin = new System.Windows.Forms.Padding(4);
             this.imprimerButton.Name = "imprimerButton";
-            this.imprimerButton.Size = new System.Drawing.Size(520, 52);
+            this.imprimerButton.Size = new System.Drawing.Size(715, 78);
             this.imprimerButton.TabIndex = 4;
             this.imprimerButton.Text = "Imprimer la liste des animes";
             this.imprimerButton.UseVisualStyleBackColor = true;
+            this.imprimerButton.Click += new System.EventHandler(this.imprimerButton_Click);
             // 
             // rechercheGroupBox
             // 
-            this.rechercheGroupBox.Controls.Add(this.button1);
+            this.rechercheGroupBox.Controls.Add(this.rechercheButton);
             this.rechercheGroupBox.Controls.Add(this.rechercherAnimeTextBox);
             this.rechercheGroupBox.Controls.Add(this.rechercherAnimelabel);
-            this.rechercheGroupBox.Location = new System.Drawing.Point(32, 504);
+            this.rechercheGroupBox.Location = new System.Drawing.Point(44, 756);
+            this.rechercheGroupBox.Margin = new System.Windows.Forms.Padding(4);
             this.rechercheGroupBox.Name = "rechercheGroupBox";
-            this.rechercheGroupBox.Size = new System.Drawing.Size(520, 169);
+            this.rechercheGroupBox.Padding = new System.Windows.Forms.Padding(4);
+            this.rechercheGroupBox.Size = new System.Drawing.Size(715, 254);
             this.rechercheGroupBox.TabIndex = 5;
             this.rechercheGroupBox.TabStop = false;
             this.rechercheGroupBox.Text = "Recherche:";
             // 
-            // button1
+            // rechercheButton
             // 
-            this.button1.Location = new System.Drawing.Point(18, 91);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(482, 52);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Afficher le nombre d\'anime";
-            this.button1.UseVisualStyleBackColor = true;
+            this.rechercheButton.Location = new System.Drawing.Point(25, 136);
+            this.rechercheButton.Margin = new System.Windows.Forms.Padding(4);
+            this.rechercheButton.Name = "rechercheButton";
+            this.rechercheButton.Size = new System.Drawing.Size(663, 78);
+            this.rechercheButton.TabIndex = 6;
+            this.rechercheButton.Text = "Afficher le nombre d\'anime";
+            this.rechercheButton.UseVisualStyleBackColor = true;
+            this.rechercheButton.Click += new System.EventHandler(this.rechercheButton_Click);
             // 
             // rechercherAnimeTextBox
             // 
             this.rechercherAnimeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.rechercherAnimeTextBox.Location = new System.Drawing.Point(18, 58);
+            this.rechercherAnimeTextBox.Location = new System.Drawing.Point(25, 87);
+            this.rechercherAnimeTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.rechercherAnimeTextBox.Multiline = true;
             this.rechercherAnimeTextBox.Name = "rechercherAnimeTextBox";
-            this.rechercherAnimeTextBox.Size = new System.Drawing.Size(482, 27);
+            this.rechercherAnimeTextBox.Size = new System.Drawing.Size(663, 40);
             this.rechercherAnimeTextBox.TabIndex = 7;
             // 
             // rechercherAnimelabel
             // 
             this.rechercherAnimelabel.AutoSize = true;
-            this.rechercherAnimelabel.Location = new System.Drawing.Point(15, 35);
+            this.rechercherAnimelabel.Location = new System.Drawing.Point(21, 52);
+            this.rechercherAnimelabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.rechercherAnimelabel.Name = "rechercherAnimelabel";
-            this.rechercherAnimelabel.Size = new System.Drawing.Size(268, 16);
+            this.rechercherAnimelabel.Size = new System.Drawing.Size(399, 25);
             this.rechercherAnimelabel.TabIndex = 6;
             this.rechercherAnimelabel.Text = "Type d\'anime recherche: (Shōnen et Shojo):";
             // 
             // animeListePictureBox
             // 
             this.animeListePictureBox.Image = global::AnimeListe.Properties.Resources.Big3;
-            this.animeListePictureBox.Location = new System.Drawing.Point(117, 47);
+            this.animeListePictureBox.Location = new System.Drawing.Point(161, 70);
+            this.animeListePictureBox.Margin = new System.Windows.Forms.Padding(4);
             this.animeListePictureBox.Name = "animeListePictureBox";
-            this.animeListePictureBox.Size = new System.Drawing.Size(370, 180);
+            this.animeListePictureBox.Size = new System.Drawing.Size(509, 270);
             this.animeListePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.animeListePictureBox.TabIndex = 1;
             this.animeListePictureBox.TabStop = false;
             // 
+            // animeListePrintDocument
+            // 
+            this.animeListePrintDocument.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.animeListePrintDocument_PrintPage);
+            // 
+            // animeListePrintPreviewDialog
+            // 
+            this.animeListePrintPreviewDialog.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.animeListePrintPreviewDialog.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.animeListePrintPreviewDialog.ClientSize = new System.Drawing.Size(400, 300);
+            this.animeListePrintPreviewDialog.Enabled = true;
+            this.animeListePrintPreviewDialog.Icon = ((System.Drawing.Icon)(resources.GetObject("animeListePrintPreviewDialog.Icon")));
+            this.animeListePrintPreviewDialog.Name = "animeListePrintPreviewDialog";
+            this.animeListePrintPreviewDialog.Visible = false;
+            // 
             // AnimeListeForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 698);
+            this.ClientSize = new System.Drawing.Size(793, 1047);
             this.Controls.Add(this.rechercheGroupBox);
             this.Controls.Add(this.imprimerButton);
             this.Controls.Add(this.typeAnimeRichTextBox);
@@ -149,9 +178,11 @@
             this.Controls.Add(this.animeListePictureBox);
             this.Controls.Add(this.animeListeMenuStrip);
             this.MainMenuStrip = this.animeListeMenuStrip;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AnimeListeForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Anime Liste";
+            this.Load += new System.EventHandler(this.AnimeListeForm_Load);
             this.animeListeMenuStrip.ResumeLayout(false);
             this.animeListeMenuStrip.PerformLayout();
             this.rechercheGroupBox.ResumeLayout(false);
@@ -172,8 +203,11 @@
         private System.Windows.Forms.Button imprimerButton;
         private System.Windows.Forms.GroupBox rechercheGroupBox;
         private System.Windows.Forms.Label rechercherAnimelabel;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button rechercheButton;
         private System.Windows.Forms.TextBox rechercherAnimeTextBox;
+        private System.Drawing.Printing.PrintDocument animeListePrintDocument;
+        private System.Windows.Forms.FontDialog animeListeFontDialog;
+        private System.Windows.Forms.PrintPreviewDialog animeListePrintPreviewDialog;
     }
 }
 
--- AnimeListeForm.cs
+++ AnimeListeForm.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿/*
+    Programmeur:    Manuela,dylane,andreas,cedric
+    Date:           juin 2024
+  
+    Solution:       animeListe.sln
+    Projet:         animeListe.csproj
+    Classe:         animeListeForm.cs  
+  
+    But:			modification des donnes ajout des controles sur la pages a imprimer         
+  
+    Traitement:	    Implementer le code
+   			
+    Sorties:		imprimer des controles
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Drawing.Text;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,35 +31,229 @@ namespace AnimeListe
 {
     public partial class AnimeListeForm : Form
     {
+        #region Initialisation
         public AnimeListeForm()
         {
             InitializeComponent();
         }
+        #endregion
 
+        #region Load
         private void AnimeListeForm_Load(object sender, EventArgs e)
         {
-            policesToolStripComboBox.SelectedIndexChanged -= policesToolStripComboBox_SelectedIndexChanged;
+            LoadRTFFile();
+            animeListePrintPreviewDialog.Width = SystemInformation.VirtualScreen.Width;
+            animeListePrintPreviewDialog.Height = SystemInformation.VirtualScreen.Height;
+            animeListePrintPreviewDialog.PrintPreviewControl.Zoom = 2;
+
+           policesToolStripComboBox.SelectedIndexChanged -= policesToolStripComboBox_SelectedIndexChanged;
             AfficherPolicesInstallees();
             policesToolStripComboBox.SelectedIndexChanged += policesToolStripComboBox_SelectedIndexChanged;
+
         }
-        private void policesToolStripComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
+
+        #endregion
+
+        #region police instaler
         private void AfficherPolicesInstallees()
         {
             try
             {
-                InstalledFontCollection oInstalledFonts = new InstalledFontCollection();
-
-                foreach (FontFamily oFontFamily in oInstalledFonts.Families)
+                var fonts = new System.Drawing.Text.InstalledFontCollection();
+                foreach (FontFamily font in fonts.Families)
                 {
-                    policesToolStripComboBox.Items.Add(oFontFamily.Name);
+                    policesToolStripComboBox.Items.Insert(0, font.Name);
                 }
+                
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
         }
+
+        #endregion
+
+        #region Police
+        private void policesToolStripComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // Apply the selected font to the RichTextBox content
+                typeAnimeRichTextBox.SelectionFont = new Font(policesToolStripComboBox.SelectedItem.ToString(), typeAnimeRichTextBox.SelectionFont.Size);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error applying font: {ex.Message}");
+            }
+        }
+        #endregion
+
+        #region rtf
+
+        private string rtfFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data\\document.rtf");
+
+        private void LoadRTFFile()
+        {
+            try
+            {
+                if (File.Exists(rtfFilePath))
+                {
+                    typeAnimeRichTextBox.LoadFile(rtfFilePath, RichTextBoxStreamType.PlainText);
+
+                    string firstLineText = "Dragon Ball";
+                    int startIndex = typeAnimeRichTextBox.Find(firstLineText);
+                    if (startIndex != -1)
+                    {
+                        typeAnimeRichTextBox.Select(startIndex ,firstLineText.Length); // Sélectionner la première ligne
+                    }
+
+                    // Optionnel : Si tu veux défiler jusqu'à la sélection
+                    typeAnimeRichTextBox.ScrollToCaret();
+
+                }
+                else
+                {
+                    MessageBox.Show($"Le fichier RTF n'a pas été trouvé : {rtfFilePath}");
+                }
+            }
+            catch (Exception )
+            {
+                 MessageBox.Show($"Erreur lors du chargement du fichier RTF ");
+            }
+        }
+
+
+
+
+
+        #endregion
+
+        #region rechercher
+        private void rechercheButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string motChercher = rechercherAnimeTextBox.Text;
+                if (!string.IsNullOrEmpty(motChercher))
+                {
+                    int startIndex = 0;
+                    int nbOccurence = 0;
+
+                    while ((startIndex = typeAnimeRichTextBox.Find(motChercher, startIndex, RichTextBoxFinds.None)) != -1)
+                    {
+                        nbOccurence++;
+                        startIndex += motChercher.Length;  // Move index forward
+                    }
+
+                    MessageBox.Show($"Le mot '{motChercher}' est apparu {nbOccurence} fois.");
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez entrer un mot à rechercher.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during search: {ex.Message}");
+            }
+
+        }
+
+
+
+
+        #endregion
+
+        #region imprimer
+
+        private void imprimerButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                animeListePrintPreviewDialog.Document = animeListePrintDocument;
+                animeListePrintPreviewDialog.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during printing: {ex.Message}");
+            }
+
+
+        }
+        private void animeListePrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+
+            try
+            {
+                Image imageChoisie;
+                imageChoisie = Properties.Resources.Big3;
+                // Utilise la taille du PictureBox directement pour l'image imprimée
+                int imgWidth = animeListePictureBox.Width;
+                int imgHeight = animeListePictureBox.Height;
+
+                // Calcul des coordonnées pour centrer l'image
+                int posX = (int)((e.MarginBounds.Width) / 2);
+                int posY = 50;
+
+                // Dessine l'image centrée avec les dimensions du PictureBox
+                e.Graphics.DrawImage(animeListePictureBox.Image, posX, posY, imgWidth, imgHeight);
+
+                
+
+                Font enteteFont, detailFont;
+
+                enteteFont = new Font("Arial", 18.0F, FontStyle.Bold);
+                detailFont = new Font("Arial", 14.0F);
+
+                //definir les tailles par rapport au police
+
+                Single hauteurPoliceEntete = enteteFont.GetHeight();
+               
+
+                //variable
+                string titre = "liste anime ";
+                string entete = "Type manga       Nom manga";
+                //mesurer le titre 
+
+                Single largeurTitreSingle = e.Graphics.MeasureString(titre, enteteFont).Width;
+
+
+                
+                //position du crayon
+
+                float XFloat = e.MarginBounds.X;
+                float YFloat = e.MarginBounds.Y;
+
+                //imprimr le titre et souligner
+                e.Graphics.DrawString(titre, enteteFont, Brushes.Blue, XFloat+280, YFloat+120);
+                YFloat += hauteurPoliceEntete;
+                e.Graphics.DrawLine(new Pen(Color.Blue, 3.0F), XFloat + 280, YFloat+120, XFloat + largeurTitreSingle+280, YFloat+120);
+
+
+                //sous tutre goup box
+                YFloat += 130;
+
+                e.Graphics.DrawString(entete, detailFont, Brushes.Red, XFloat, YFloat);
+                YFloat += hauteurPoliceEntete * 2;
+                
+
+
+                string content = typeAnimeRichTextBox.Rtf;
+                // Custom logic to format and print the content
+                e.Graphics.DrawString(typeAnimeRichTextBox.Text, new Font("Arial", 12), Brushes.Black, new PointF(100,YFloat-20));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"erreur lors de l impressiong: {ex.Message}");
+            }
+        }
+
+
+
+
+        #endregion
+
+        
     }
 }
